33c8966ca7203b68f9cd82135cf0a2a8351fc187
MarkinAO <habar77@yandex.ru>
2023-04-14 17:09:57 +0700

    lint fix

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,37 +1,38 @@
 import orderByProps from '../js/main';
 
 test('Test orderByProps', () => {
-  const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
-  
-  let res = [
-    {key: "name", value: "мечник"}, // порядок взят из массива с ключами
-    {key: "level", value: 2}, // порядок взят из массива с ключами
-    {key: "attack", value: 80}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
-    {key: "defence", value: 40}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
-    {key: "health", value: 10} // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
-  ]
-  expect(orderByProps(obj, ["name", "level"])).toEqual(res);
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
 
-  res = [    
-    {key: "attack", value: 80},
-    {key: "defence", value: 40},
-    {key: "health", value: 10},
-    {key: "level", value: 2},
-    {key: "name", value: "мечник"}
-  ]
+  let res = [
+    { key: 'name', value: 'мечник' }, // порядок взят из массива с ключами
+    { key: 'level', value: 2 }, // порядок взят из массива с ключами
+    { key: 'attack', value: 80 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
+    { key: 'defence', value: 40 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
+    { key: 'health', value: 10 }, // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
+  ];
+  expect(orderByProps(obj, ['name', 'level'])).toEqual(res);
+
+  res = [
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ];
   expect(orderByProps(obj)).toEqual(res);
 
-  res = [    
-    {key: "health", value: 10},
-    {key: "attack", value: 80},
-    {key: "defence", value: 40},
-    {key: "name", value: "мечник"},
-    {key: "level", value: 2}    
-  ]
-  expect(orderByProps(obj, ["health", "attack", "defence", "name", "level"])).toEqual(res);
+  res = [
+    { key: 'health', value: 10 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+  ];
+  expect(orderByProps(obj, ['health', 'attack', 'defence', 'name', 'level'])).toEqual(res);
 
   expect(() => {
-    orderByProps()
+    orderByProps();
   }).toThrow('Функция вызвана без объекта!');
 });
-
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,20 @@
 export default function orderByProps(object, sortRules) {
-  if(!object) {
-    throw new Error('Функция вызвана без объекта!')
+  if (!object) {
+    throw new Error('Функция вызвана без объекта!');
   }
-  let sort = sortRules ? sortRules : []
-  
-  let defaultSorting = []
-  let customSorting = []
+  const sort = sortRules || [];
+
+  const defaultSorting = [];
+  const customSorting = [];
 
   for (const key in object) {
     if (sort.includes(key)) {
-      customSorting[sort.findIndex(element => element === key)] = {'key': key, value: object[key]}
+      customSorting[sort.findIndex((element) => element === key)] = { key, value: object[key] };
     } else {
-      defaultSorting.push({'key': key, value: object[key]})
-    }    
+      defaultSorting.push({ key, value: object[key] });
+    }
   }
 
-  defaultSorting.sort((a, b) => a.key.localeCompare(b.key))
-  return [...customSorting, ...defaultSorting]
+  defaultSorting.sort((a, b) => a.key.localeCompare(b.key));
+  return [...customSorting, ...defaultSorting];
 }
